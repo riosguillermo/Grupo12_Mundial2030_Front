cbf5e52300112c95a3b396376280ecc3536b33c6
Roni <ronimalamud@gmail.com>
2023-10-17 21:12:14 -0300

    3 componentes terminados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import SideNavbar from "./components/sideNavBar";
+import Topbar from "./components/topbar"
+import Heading from "./components/heading"
 
 function App() {
   return (
     <div>
+      <Topbar />
+      <Heading />
       <SideNavbar />
     </div>
   );
